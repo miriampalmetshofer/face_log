9a1560fa78c7b4822eae8f7b1e3d0070b60564d9
miriampalmetshofer <miriam.palmetshofer@students.fh-hagenberg.at>
2026-06-13 12:36:36 +0200

    Fix inout field displaying characters backwards

--- lib/widgets/form_fields/text_input_field.dart
+++ lib/widgets/form_fields/text_input_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../models/annotation_category.dart';
 
-class TextInputField extends StatelessWidget {
+class TextInputField extends StatefulWidget {
   final AnnotationCategory category;
   final String? currentValue;
   final ValueChanged<String> onChanged;
@@ -14,12 +14,41 @@ class TextInputField extends StatelessWidget {
   });
 
   @override
+  State<TextInputField> createState() => _TextInputFieldState();
+}
+
+class _TextInputFieldState extends State<TextInputField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.currentValue ?? '');
+  }
+
+  @override
+  void didUpdateWidget(TextInputField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Only update if the value changed externally (not from user input)
+    if (widget.currentValue != oldWidget.currentValue &&
+        widget.currentValue != _controller.text) {
+      _controller.text = widget.currentValue ?? '';
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
         decoration: InputDecoration(
-          labelText: category.title,
+          labelText: widget.category.title,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
           ),
@@ -28,8 +57,8 @@ class TextInputField extends StatelessWidget {
             vertical: 12.0,
           ),
         ),
-        controller: TextEditingController(text: currentValue ?? ''),
-        onChanged: onChanged,
+        controller: _controller,
+        onChanged: widget.onChanged,
       ),
     );
   }
